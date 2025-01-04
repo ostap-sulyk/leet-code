f262775020d562d7bc97681d8ebbcf46b5afe955
Ostap Sulyk <ostapsulyk@gmail.com>
2025-01-04 11:27:21 -0500

    Remove Duplicates From Sorted Array

--- problems/easy/RemoveDuplicatesFromSortedArray/solution.go
+++ problems/easy/RemoveDuplicatesFromSortedArray/solution.go
@@ -3,7 +3,7 @@ package removeduplicatesfromsortedarray
 func removeDuplicates(nums []int) int {
 	k := 1
 	largest := nums[0]
-	for i := 0; i < len(nums); i++ {
+	for i := 1; i < len(nums); i++ {
 		if nums[i] > largest {
 			nums[k], largest = nums[i], nums[i]
 			k++
